33607cfb452d4037dd67f68824ad62967355c601
affangitty <affanmwa@gmail.com>
2026-06-23 10:32:42 +0530

    Initial Angular weather app

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeatherForecast, WeatherService } from './weather.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  forecasts: WeatherForecast[] = [];
+  error = '';
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getForecast().subscribe({
+      next: (data) => (this.forecasts = data),
+      error: () => (this.error = 'Could not load weather data. Is the API running on http://localhost:5156?')
+    });
+  }
+}
---
+++ src/app/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class WeatherService {
+  private readonly apiUrl = 'http://localhost:5156/WeatherForecast';
+
+  constructor(private http: HttpClient) {}
+
+  getForecast(): Observable<WeatherForecast[]> {
+    return this.http.get<WeatherForecast[]>(this.apiUrl);
+  }
+}
